0e01af1b74c6dd14cace687fb1a97a9cb9cb538b
Mariam Senzia <mariamsenzia@gmail.com>
2025-10-20 08:52:07 +0300

    Initial project set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider, Heading } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <ChakraProvider>
+        <Heading>Hello World</Heading>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
